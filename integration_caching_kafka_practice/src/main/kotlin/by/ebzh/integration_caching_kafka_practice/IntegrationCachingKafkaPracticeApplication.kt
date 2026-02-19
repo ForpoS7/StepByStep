296823dcafd5289732b98be5097ca1c389c25e41
Кирилл <sigma@gg>
2026-02-19 13:24:04 +0300

    added kafka tasks

---
+++ integration_caching_kafka_practice/src/main/kotlin/by/ebzh/integration_caching_kafka_practice/IntegrationCachingKafkaPracticeApplication.kt
@@ -0,0 +1,11 @@
+package by.ebzh.integration_caching_kafka_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegrationCachingKafkaPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<IntegrationCachingKafkaPracticeApplication>(*args)
+}
---
+++ integration_caching_kafka_practice/src/test/kotlin/by/ebzh/integration_caching_kafka_practice/IntegrationCachingKafkaPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package by.ebzh.integration_caching_kafka_practice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IntegrationCachingKafkaPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
